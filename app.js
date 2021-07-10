709e9106d8d287da65eb29b5c17fcbe21cd2f9b0
nndeto <n_ndeto@yahoo.com>
2021-07-10 16:32:24 -0400

    verbal questions answered

---
+++ app.js
@@ -0,0 +1,8 @@
+//Verbal Questions
+//Q1 - Parameters are the condtions you set in the function's parentheses when you're first creating the function. 
+//-Arguments are what you pass into the parentheses when you call the function
+//Q2 - Console.log will print whatever you pass into it directly. Return will give back the answer/outcome, but it's 
+//in a sort of hold. To see/access the answer you'll need to console.log it or pass it into a variable and act
+//upon the variable.
+//Q3 Being able to return a value makes a function more versatile in its uses.
+
